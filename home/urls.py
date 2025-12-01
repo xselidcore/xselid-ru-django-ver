a947d3056757553d085d5d8018390a7441d65ef0
xselid <gavrilovnikita720@gmail.com>
2025-12-01 22:45:54 +0500

    robots and sitmap

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import index
+from .views import index, robots_txt
 
 urlpatterns = [
     path('', index, name='home'),
+    path('robots.txt', robots_txt, name='robots_txt'),
 ]
--- xselid_ru/urls.py
+++ xselid_ru/urls.py
@@ -1,12 +1,20 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
 from django.conf.urls import handler404
+from django.contrib.sitemaps.views import sitemap
+
 from home import views
+from home.sitemaps import StaticViewSitemap
+
+
+sitemaps = {
+    "static": StaticViewSitemap,
+}
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
+    path('sitemap.xml', sitemap, {"sitemaps": sitemaps}, name='sitemap'),
 ]
 
 
